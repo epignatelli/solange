b87b5301ceef9366c8ca4f62cfdb4dbc44683430
Eduardo <edu.pignatelli@gmail.com>
2024-11-25 19:17:37 +0000

    test

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 # This file sets up a new agave voting validator
+# building solana from source. This should optimise
+# the validator for the specs of each machine 
 
 # set versions
 SOL_VERSION="2.0.16"
@@ -11,6 +13,9 @@ cd solana
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+# add cargo to path
+export PATH="$HOME/.cargo/bin:$PATH"
+
 # install base prerequisites
 apt-get install \
     build-essential \
@@ -18,21 +23,17 @@ apt-get install \
     libudev-dev llvm libclang-dev \
     protobuf-compiler
 
-# fetch and inflate platform-tools
-wget https://github.com/anza-xyz/platform-tools/archive/refs/tags/v$TOOLS_VERSION.tar.gz
-tar xvf "v$TOOLS_VERSION".tar.gz
-
-# build platform-tools
-cd platform-tools-$TOOLS_VERSION
-chmod 777 ./build.sh
-./build.sh
 
 # fetch and inflate solana source
 wget https://github.com/anza-xyz/agave/archive/refs/tags/v$SOL_VERSION.tar.gz
 tar xvf "$SOL_VERSION.tar.gz"
 
+# cd to agave
+cd agave-$SOL_VERSION
+
 # build
 ./scripts/cargo-install-all.sh .
+
+# add agave to path
 export PATH=$PWD/bin:$PATH
-cd agave-$SOL_VERSION
-agave-install init
+agave-install init $SOL_VERSION
---
+++ setup_validator.sh
@@ -0,0 +1,1 @@
+solana config set --url https://api.testnet.solana.com  # todo: make this a variable
